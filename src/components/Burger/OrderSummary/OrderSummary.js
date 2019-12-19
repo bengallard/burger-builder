937a68f5e61612d0c208de5d741726b63c2f517f
Ben Gallard <bengallard@gmail.com>
2019-12-19 12:59:52 +0000

    lecture 172: Order summary price

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,7 +19,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
-            <p>Continue to Checkout</p>
+        <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
         </Aux>

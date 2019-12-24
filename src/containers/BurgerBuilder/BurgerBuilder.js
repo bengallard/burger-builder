aa701727361b49f90673551c5ce1b879a5d81fd2
Ben Gallard <bengallard@gmail.com>
2019-12-24 18:08:41 +0000

    lecture 211: Send POST request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -100,7 +100,8 @@ class BurgerBuilder extends Component {
             deliveryMethod: 'fastest'
         }
         axios.post('/orders.json', order)
-            .then(response => console.log(response))
+            .th
+            en(response => console.log(response))
             .catch(error => console.log(error))
     }
 

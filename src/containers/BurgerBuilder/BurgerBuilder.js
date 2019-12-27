f784b695fe27bd5963b720ffd0dc8e09757f007a
Ben Gallard <bengallard@gmail.com>
2019-12-27 19:04:33 +0000

    lecture 215: Remove/eject interceptors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -43,12 +43,12 @@ class BurgerBuilder extends Component {
 
     updatePurchaseState(ingredients) {
         const sum = Object.keys(ingredients)
-        .map(igKey => {
-            return ingredients[igKey]
-        })
-        .reduce((sum, el) => {
-            return sum + el
-        }, 0)
+            .map(igKey => {
+                return ingredients[igKey]
+            })
+            .reduce((sum, el) => {
+                return sum + el
+            }, 0)
         this.setState({purchaseable: sum > 0})
     }
 

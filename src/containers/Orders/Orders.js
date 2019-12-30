fda359c792190a01769310e30ebfb0b3de9a6a5c
Ben Gallard <bengallard@gmail.com>
2019-12-30 13:46:46 +0000

    lecture 257: Orders Page

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+// import classes from './Orders.css'
+import Order from '../../components/Order/Order'
+
+class Orders extends Component {
+    render() {
+        return (
+            <div>
+                <Order />
+                <Order />
+            </div>
+        )
+    }
+}
+
+export default Orders

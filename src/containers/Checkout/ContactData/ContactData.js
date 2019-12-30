66d34b0e43a428acde272ae84b2e437261b5a515
Ben Gallard <bengallard@gmail.com>
2019-12-30 14:26:36 +0000

    lecture 260: Fetch orders

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -22,7 +22,7 @@ class ContactData extends Component {
         this.setState({loading: true})
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.rice,
+            price: this.props.price,
             customer: {
                 name: 'Ben Gallard',
                 address: {

634b323ce2f15889f55d61f292b22707789e7c94
Ben Gallard <bengallard@gmail.com>
2019-12-31 10:24:41 +0000

    lecture 267-269: - Setup JSConfig for Form - Dynamically create Inputs from JSConfig - Dynamically create dropdown input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,11 +8,57 @@ import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Zip Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: ''
+            }
         },
         loading: false
     }
@@ -23,17 +69,7 @@ class ContactData extends Component {
         this.setState({loading: true})
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price,
-            customer: {
-                name: 'Ben Gallard',
-                address: {
-                    street: '45 Queensborough Tce',
-                    zipCode: 'W23SY',
-                    country: 'UK'
-                },
-                email: 'bengallard@gmail.com',
-            },
-            deliveryMethod: 'fastest'
+            price: this.props.price
         }
         axios.post('/orders.json', order)
             .then(response => {
@@ -46,12 +82,22 @@ class ContactData extends Component {
     }
 
     render() {
+        const fromElements = []
+        for (let key in this.state.orderForm) {
+            fromElements.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
         let form = (
             <form>
-                <Input inputtype='input' type='text' name='name' placeholder='Your Name' />
-                <Input inputtype='input' type='email' name='email' placeholder='Your Email' />
-                <Input inputtype='input' type='text' name='street' placeholder='Street' />
-                <Input inputtype='input' type='text' name='postal' placeholder='Postal Code' />
+                {fromElements.map(formElement => (
+                    <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.value} />
+                ))}
                 <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
             </form>
         )
